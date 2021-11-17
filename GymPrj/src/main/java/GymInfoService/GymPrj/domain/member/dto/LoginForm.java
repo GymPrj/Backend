538ac43adf5d6bba7c7a4acb06d6978d6e429372
82109 <qwe199942@gmail.com>
2021-11-18 03:10:31 +0900

    refactor: final 선언 제거

--- GymPrj/src/main/java/GymInfoService/GymPrj/domain/member/dto/LoginForm.java
+++ GymPrj/src/main/java/GymInfoService/GymPrj/domain/member/dto/LoginForm.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class LoginForm {
 
-    private final String email;
-    private final String password;
+    private String email;
+    private String password;
 
 }
